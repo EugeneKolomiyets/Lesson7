4c5ee4dac8f397c557f74016eaea1935f01c2448
EugeneKolomiyets <evgenum@gmail.com>
2021-03-19 20:18:11 +0200

    test branch commit

--- src/com/alevel/Main.java
+++ src/com/alevel/Main.java
@@ -3,6 +3,8 @@ package com.alevel;
 public class Main {
     public static void main(String[] args) {
         System.out.println("test commit");
-        System.out.println("test");
+        System.out.println("test+");
+
+        System.out.println("test branch");
     }
 }
